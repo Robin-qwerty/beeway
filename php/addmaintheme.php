a2f1d7972456834ecd183097584c829bd6cfdca2
robin-qwerty <robin@humilis.net>
2023-11-21 14:55:52 +0100

    fix adding maintheme error

--- php/addmaintheme.php
+++ php/addmaintheme.php
@@ -1,9 +1,12 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
   require_once '../private/dbconnect.php';
   session_start();
 
   if (isset($_SESSION['userid'], $_SESSION['userrole']) && ($_SESSION['userrole'] === 'superuser' || $_SESSION['userrole'] === 'admin')) {
-    try {
+    // try {
       if (empty($_POST['namethemep1']) || empty($_POST['namethemep2']) || empty($_POST['namethemep3']) || empty($_POST['namethemep4']) || empty($_POST['namethemep5']) || empty($_POST['schoolyear'])) {
         $_SESSION['namethemep1'] = $_POST['namethemep1'];
         $_SESSION['namethemep2'] = $_POST['namethemep2'];
@@ -45,8 +48,8 @@
           exit;
         }
 
-        $sql = "INSERT INTO maintheme (`schoolid`, `namethemep1`, `namethemep2`, `namethemep3`, `namethemep4`, `namethemep5`, `schoolyear`)
-                VALUES (:schoolid, :namethemep1, :namethemep2, :namethemep3, :namethemep4, :namethemep5, :schoolyear)";
+        $sql = "INSERT INTO maintheme (`schoolid`, `namethemep1`, `namethemep2`, `namethemep3`, `namethemep4`, `namethemep5`, `schoolyear`, `createdby`, `updatedby`)
+                VALUES (:schoolid, :namethemep1, :namethemep2, :namethemep3, :namethemep4, :namethemep5, :schoolyear, :createdby, :updatedby)";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':schoolid', $school->schoolid);
         $stmt->bindParam(':namethemep1', $_POST['namethemep1']);
@@ -55,6 +58,8 @@
         $stmt->bindParam(':namethemep4', $_POST['namethemep4']);
         $stmt->bindParam(':namethemep5', $_POST['namethemep5']);
         $stmt->bindParam(':schoolyear', $_POST['schoolyear']);
+        $stmt->bindParam(':createdby', $_SESSION['userid']);
+        $stmt->bindParam(':updatedby', $_SESSION['userid']);
         $stmt->execute();
 
         $lastInsertedId = $conn->lastInsertId();
@@ -88,17 +93,17 @@
           exit;
         }
       }
-    } catch (\Exception $e) {
-      $sql = 'INSERT INTO logs (userid, useragent, action, tableid, interactionid, error) VALUES (:userid, :useragent, 1, 4, 0, 5)';
-      $stmt = $conn->prepare($sql);
-      $stmt->bindParam(':userid', $_SESSION['userid']);
-      $stmt->bindValue(':useragent', $_SESSION['useragent']);
-      $stmt->execute();
+    // } catch (\Exception $e) {
+    //   $sql = 'INSERT INTO logs (userid, useragent, action, tableid, interactionid, error) VALUES (:userid, :useragent, 1, 4, 0, 5)';
+    //   $stmt = $conn->prepare($sql);
+    //   $stmt->bindParam(':userid', $_SESSION['userid']);
+    //   $stmt->bindValue(':useragent', $_SESSION['useragent']);
+    //   $stmt->execute();
 
-      $_SESSION['error'] = 'An error occurred. Please try again. or contact an admin if this keeps happening';
-      header('Location: ../index.php?page=hoofdthemalijst');
-      exit;
-    }
+    //   $_SESSION['error'] = 'An error occurred. Please try again. or contact an admin if this keeps happening';
+    //   header('Location: ../index.php?page=hoofdthemalijst');
+    //   exit;
+    // }
   } else {
     $sql = 'INSERT INTO logs (userid, useragent, action, tableid, interactionid, error) VALUES (9999, :useragent, 1, 4, 0, 1)';
     $stmt = $conn->prepare($sql);
